4f6ff1e0c75e4757986d55d294c9fb782cd2ba3e
RING-NOW <ringnowvirtual@gmail.com>
2026-06-19 10:04:18 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const RINGNOW_CONFIG = {
   estructura: {
     pisos:         0,
     deptosPorPiso: 0,
-    deptosManual:  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, "Enc"],
+    deptosManual:  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
   },
 
   /* ── Firebase ── */
